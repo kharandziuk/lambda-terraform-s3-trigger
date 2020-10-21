9d1ef51a482b510a5ad2925869d2bd50fc29734c
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-10-21 18:50:36 +0200

    add policy attachment

--- main.tf
+++ main.tf
@@ -31,13 +31,15 @@ locals {
 resource "aws_lambda_function" "test_lambda" {
   filename         = local.dist_path
   function_name    = "test_lambda"
-  role             = aws_iam_role.iam_for_lambda_tf.arn
+  role             = aws_iam_role.iam_for_lambda.arn
   handler          = "index.handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime = "nodejs12.x"
 }
 
-resource "aws_iam_role" "iam_for_lambda_tf" {
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda_tf"
 
   assume_role_policy = <<EOF
@@ -55,6 +57,26 @@ resource "aws_iam_role" "iam_for_lambda_tf" {
   ]
 }
 EOF
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "logs-attachment"
+  roles      = [aws_iam_role.iam_for_lambda.name]
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+  # fix account id
+  policy = templatefile(
+    "${path.module}/logs-policy-attachment.json.tmpl",
+    {
+      name: aws_lambda_function.test_lambda.function_name,
+      account_id: data.aws_caller_identity.current.account_id,
+      region: var.aws_region
+    }
+  )
 }
 
 resource "aws_lambda_permission" "allow_terraform_bucket" {
